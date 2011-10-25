622788e305561c57bfe1e931380bdb72948b95f5
David van Geest <david.vangeest@gmail.com>
2011-10-25 12:09:46 -0700

    Allow trailing slash in URLs.

--- helpers.rb
+++ helpers.rb
@@ -1,6 +1,6 @@
 helpers do
   def menu_item(text, href)
-    if request.path_info == href
+    if request.path_info.chomp('/') == href
       "<li class=\"cur\">#{text}</li>"
     else
       "<li><a href=\"#{href}\">#{text}</a></li>"
