0cde1f573fcfcb754331db65e8aba18c7783beb3
David van Geest <davidv@spindance.com>
2015-01-22 13:23:06 -0500

    Fix infowindow on location map.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,19 @@
 function initializeApp(pathInfo) {
   if (pathInfo == "/location") {
-    loadMap();  
+    loadMap();
   }
-  
+
   $("a.new-window").unbind("click").click(function() {
     newWindow = window.open($(this).attr('href'), 'name');
-    if (window.focus) { 
-      newWindow.focus(); 
+    if (window.focus) {
+      newWindow.focus();
     }
     return false;
   });
 }
 
 function loadMap() {
-  var markerString = "Patricia Van Reenen<br>1096 The Parkway<br>London, Ontario N6A 2X1";
+  var markerString = "<div id=\"info-window\">Patricia Van Reenen<br>1096 The Parkway<br>London, Ontario<br>N6A 2X1</div>";
   var latLng = new google.maps.LatLng(43.004629, -81.267193);
   var options = {
     zoom: 12,
@@ -21,20 +21,20 @@ function loadMap() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map"), options);
-  
+
   var marker = new google.maps.Marker({
-      position: latLng, 
-      map: map, 
+      position: latLng,
+      map: map,
       title: markerString
   });
-  
+
   var infoWindow = new google.maps.InfoWindow({
-    content: markerString
+    content: markerString,
   });
-  
+
   google.maps.event.addListener(marker, 'click', function() {
     infoWindow.open(map, marker);
   });
-  
+
   infoWindow.open(map, marker);
 }
